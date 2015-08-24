2920863e32b20012432914df29a15e4c32d74c08
Elliott Hughes-Taylor <ehughestaylor@Elliotts-MacBook-Pro.local>
2015-08-24 11:32:45 -0400

    overlay fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+
+
     particlesJS("particles-js", {
       "particles": {
         "number": {
